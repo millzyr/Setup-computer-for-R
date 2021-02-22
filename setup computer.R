770e75ce5a4874af2aaade9cb2c31d5fd4c16643
markbneal <mark.neal@dairynz.co.nz>
2021-02-23 10:38:21 +1300

    script to copy in recommended settings

--- setup computer.R
+++ setup computer.R
@@ -4,6 +4,18 @@
 #eg user/docs/R_projects
 #Never save workspace or history (see screenshot of settings)
 #"Setup computer for R - RStudio Rstudio settings  R_projects_.png"
+#NB We have made a script that copies sensible recommended defaults automagically
+
+username <- Sys.getenv('username')
+appdata <- Sys.getenv('appdata')
+
+documents_folder <- paste0('C:\\Users\\',username,'\\Documents\\')
+my_appdata_path <- paste0(appdata, '\\Rstudio\\')
+
+urlfile="https://raw.githubusercontent.com/millzyr/Setup-computer-for-R/master/recommended-rstudio-prefs.json"
+download.file(urlfile, paste0(my_appdata_path, "rstudio-prefs.json"))
+
+
 #You may want to set up or connect to shinyapps or rsconnect to publish examples
 
 
